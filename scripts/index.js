b68dbbdc05a9b286f8b1813ca62d186d249ac4be
khalilhamdii <khaalil.hamdi@gmail.com>
2020-12-02 13:25:40 +0100

    organise files into folders + minor chagnes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let myLibrary = [{title: "book1", author: "author1", page: 20, status: false}];
+let myLibrary = [{title: "book1", author: "author1", page: 20, status: true}];
 
 function Book(title, author, pages, status ) {
   this.title = title
@@ -16,6 +16,12 @@ function addBookToLibrary(title, author, pages, status) {
   myLibrary.push(obj);
 }
 
+function showBooks() {
+
+  let checkBook = (status) => {
+    return status ? "Already read" : "Not read yet";
+  };
+  
 for(let i=0; i<myLibrary.length; i++) {
     const container = document.querySelector(".container");
     container.innerHTML = `
@@ -25,23 +31,16 @@ for(let i=0; i<myLibrary.length; i++) {
       <h4 class="card-title"><a>`+myLibrary[i].title+`</a></h4>
       <h6 card="card-author">`+myLibrary[i].author+`</h6>
       <p class="card-text">`+myLibrary[i].page+`</p>
-      <p class="card-text">`+myLibrary[i].status+`</p>
+      <p class="card-text">`+checkBook(myLibrary[i].status)+`</p>
     </div>
   
   </div>
     `
-    // const card = document.createElement('div');
-    // card.setAttribute('class', 'card');
-    // const card_body = document.createElement('div');
-    // card_body.setAttribute('class', 'card-body');
-    // const card_title = document.createElement('h4');
-    // card_title.innerHt
-    // const card_author = document.createElement('h6');
-    // card_author.setAttribute('class', 'card-author');
-    // const card_pages = document.createElement('h6');
-    // card_pages.setAttribute('class', 'card-pages');
-    // const status = document.createElement('h6');
-    // status.setAttribute('class', 'card-status');
 }
+}
+showBooks();
 
 
+const renderForm = () => {
+  
+}
